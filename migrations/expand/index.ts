4b1d773a8df680cfc77c74baa1da87c3aa17c82b
Jon Burbridge <jonathan.burbridge@sanity.io>
2026-01-29 15:13:26 +0000

    updated migration scripts

--- migrations/expand/index.ts
+++ migrations/expand/index.ts
@@ -1,16 +1,40 @@
-import {at, defineMigration, set} from 'sanity/migrate'
+import {createClient} from '@sanity/client'
+import {getPublishedId} from 'sanity'
+import {at, createOrReplace, defineMigration, patch, set} from 'sanity/migrate'
 
 export default defineMigration({
   title: 'expand',
   documentTypes: ['collection'],
 
-  migrate: {
-    document(doc, context) {
-      // this will be called for every document of the matching type
-      // any patch returned will be applied to the document
-      // you can also return mutations that touches other documents
-      const {title: currentTitle} = doc
-      return at('title', set(`${currentTitle} ${currentTitle}`))
-    },
+  async *migrate(documents) {
+    const client = createClient({
+      projectId: process.env.SANITY_STUDIO_SANITY_PROJECT_ID,
+      dataset: process.env.SANITY_STUDIO_SANITY_DATASET,
+      useCdn: false, // set to `false` to bypass the edge cache
+      apiVersion: '2025-02-19', // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
+      token: process.env.SANITY_AUTH_TOKEN, // Needed for creating a release
+    })
+
+    const release = await client.releases.create({
+      metadata: {
+        title: 'Product SEO Descriptions Update',
+        releaseType: 'undecided',
+        description: 'Bulk update of SEO descriptions based on product slugs',
+      },
+    })
+    const patches = []
+
+    for await (const document of documents()) {
+      const releaseDocumentId = `versions.${release.releaseId}.${getPublishedId(document._id)}`
+      const newDoc = {
+        ...document,
+        _id: releaseDocumentId,
+        title: `${document.title} ${document.title}`,
+        market: 'US',
+        // other new, updated, or fields you want to carry over
+      }
+      patches.push(createOrReplace(newDoc))
+    }
+    yield patches
   },
 })
--- migrations/import/index.ts
+++ migrations/import/index.ts
@@ -1,3 +1,4 @@
+import {createClient} from '@sanity/client'
 import {defineMigration, createOrReplace} from 'sanity/migrate'
 
 /**
@@ -7,11 +8,26 @@ export default defineMigration({
   title: 'import',
 
   async *migrate() {
+    const client = createClient({
+      projectId: process.env.SANITY_STUDIO_SANITY_PROJECT_ID,
+      dataset: process.env.SANITY_STUDIO_SANITY_DATASET,
+      useCdn: false, // set to `false` to bypass the edge cache
+      apiVersion: '2025-02-19', // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
+      token: process.env.SANITY_AUTH_TOKEN, // Needed for creating a release
+    })
+
+    const release = await client.releases.create({
+      metadata: {
+        title: 'Product SEO Descriptions Update',
+        releaseType: 'undecided',
+        description: 'Bulk update of SEO descriptions based on product slugs',
+      },
+    })
     const docs = []
     // get data from external source instead of this loop
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 999; i++) {
       docs.push({
-        _id: `doc-${i}`,
+        _id: `versions.${release.releaseId}.doc-${i}`,
         _type: 'collection',
         title: `Document ${i}`,
       })
@@ -19,8 +35,6 @@ export default defineMigration({
     yield docs.map((doc) => createOrReplace(doc))
   },
 })
-
-
 
 // {
 //   "result": [
