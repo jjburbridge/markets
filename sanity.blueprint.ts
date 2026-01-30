a069a3c6f8d874becc943d3430a8407e8929f9b7
Jon Burbridge <jonathan.burbridge@sanity.io>
2026-01-30 11:04:26 +0000

    updated function to use correct dataset

--- sanity.blueprint.ts
+++ sanity.blueprint.ts
@@ -2,22 +2,22 @@ import {defineBlueprint, defineDocumentFunction} from '@sanity/blueprints'
 
 export default defineBlueprint({
   resources: [
-    defineDocumentFunction({
-      name: 'translation-sync',
-      event: {
-        on: ['update'],
-        filter: '_type == "home"',
-        projection: '{_id, _type, "before": before(), "after": after()}',
-      },
-    }),
-    defineDocumentFunction({name: 'scrape', event: {on: ['create']}}),
+    // defineDocumentFunction({
+    //   name: 'translation-sync',
+    //   event: {
+    //     on: ['update'],
+    //     filter: '_type == "home"',
+    //     projection: '{_id, _type, "before": before(), "after": after()}',
+    //   },
+    // }),
+    // defineDocumentFunction({name: 'scrape', event: {on: ['create']}}),
     defineDocumentFunction({
       name: 'comments-notification',
       event: {
         on: ['create', 'update'],
         resource: {
           type: 'dataset',
-          id: `${process.env.SANITY_STUDIO_SANITY_PROJECT_ID}.${process.env.SANITY_STUDIO_SANITY_DATASET}`,
+          id: `${process.env.SANITY_STUDIO_SANITY_PROJECT_ID}.${process.env.SANITY_STUDIO_SANITY_DATASET}-comments`,
         },
         filter: '_type == "comment" && count(message[].children[_type == "mention"]) > 0',
         projection: '{..., "mentions":message[].children[_type == "mention"]}',
