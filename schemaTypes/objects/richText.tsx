885d4eff152b1adde9d47f3a092ac74d8a5bbc90
Jon Burbridge <jonathan.burbridge@sanity.io>
2026-03-20 08:56:38 +0000

    working on it

--- schemaTypes/objects/richText.tsx
+++ schemaTypes/objects/richText.tsx
@@ -15,12 +15,49 @@ export const richText = defineField({
         {name: 'dueDate', type: 'date'},
       ],
       components: {
+        // block: (props) => {
+        //   console.log('props', props)
+        //   return (
+        //     <div>
+        //       <FaRegBookmark />
+        //       {props.renderInlineBlock(props)}
+        //     </div>
+        //   )
+        // },
         block: (props) => {
-          console.log('props', props)
+          console.log('block props', props)
+          return <>{props.renderDefault(props)}</>
+        },
+        inlineBlock: (props) => {
+          console.log('inlineBlock props', props)
+          return <>{props.renderDefault(props)}</>
+        },
+        input: (props) => {
+          console.log('input props', props)
+          return <>{props.renderDefault(props)}</>
+        },
+        item: (props) => {
+          console.log('item props', props)
+          return <>{props.renderDefault(props)}</>
+        },
+        field: (props) => {
+          console.log('field props', props)
+          return <>{props.renderDefault(props)}</>
+        },
+        annotation: (props) => {
+          console.log('annotation props', props)
+          return <>{props.renderDefault(props)}</>
+        },
+        preview: (props) => {
+          console.log('preview props', props)
           return (
-            <button onClick={props.default}>
-              <FaRegBookmark />
-            </button>
+            <>
+              {props.actions}
+              <button onClick={() => (props.actions.props.isOpen = true)}>
+                <FaRegBookmark />
+              </button>
+              {props.renderDefault(props)}
+            </>
           )
         },
       },
